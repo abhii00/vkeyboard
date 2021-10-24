6122232b6a2ccce4cb9b8384091ab901023314f9
abhi00 <abhijit.pandit808@gmail.com>
2021-10-24 01:35:24 +0100

    added key class with intersection checker

--- vkeyboard.py
+++ vkeyboard.py
@@ -2,6 +2,10 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+#TODO add key method that allows pressing of key
+#TODO add keyboard class with multiple keys
+#TODO add finger counter to check whether 1 or multiple fingers are up
+
 class Detector:
     def __init__(self):
         #video
@@ -18,12 +22,16 @@ class Detector:
 
         self.breakLoop = False
 
+        self.boxes_coords = []
+
     def readImage(self):
         success, self.image = self.video.read()
         self.height, self.width, self.c = self.image.shape
 
     def drawResults(self, hands, boxes):
         if self.results.multi_hand_landmarks:
+            self.boxes_coords = []
+
             for hand_landmarks in self.results.multi_hand_landmarks:
                 if hands:
                     mp.solutions.drawing_utils.draw_landmarks(self.image, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS, self.styles['landmarks'], self.styles['handcons'])
@@ -37,10 +45,12 @@ class Detector:
                     y_coordinates = np.array(landmarks)[:,1]
                 
                     pad = 10
-                    x1  = int(np.min(x_coordinates) - pad)
-                    y1  = int(np.min(y_coordinates) - pad)
-                    x2  = int(np.max(x_coordinates) + pad)
-                    y2  = int(np.max(y_coordinates) + pad)
+                    x1 = int(np.min(x_coordinates) - pad)
+                    y1 = int(np.min(y_coordinates) - pad)
+                    x2 = int(np.max(x_coordinates) + pad)
+                    y2 = int(np.max(y_coordinates) + pad)
+
+                    self.boxes_coords.append([x1, y1, x2, y2])
 
                     cv2.rectangle(self.image, (x1, y1), (x2, y2), (155, 0, 255), 3, cv2.LINE_8)
 
@@ -55,12 +65,39 @@ class Detector:
     def checkBreak(self, break_key):
         if cv2.waitKey(1) & 0xFF == ord(break_key): self.breakLoop = True
 
+class Key:
+    def __init__(self, x1, y1, x2, y2, col_pressed = (255,255,255), col_default=(0,0,0)):
+        self.coords = [x1, y1, x2, y2]
+
+        self.col_pressed = col_pressed
+        self.col_default = col_default
+        self.intersect = False
+
+    def draw(self, image):
+        cv2.rectangle(image, (self.coords[0], self.coords[1]), (self.coords[2], self.coords[3]), self.col_pressed if self.intersect else self.col_default, 3, cv2.LINE_8)
+
+    def checkIntersect(self, box_coords):
+        if np.any(np.intersect1d(range(box_coords[0],box_coords[2]), range(self.coords[0], self.coords[2]))) and np.any(np.intersect1d(range(box_coords[1],box_coords[3]), range(self.coords[1], self.coords[3]))):
+            return True
+        else:
+            return False
+
+    def checkPressed(self, boxes_coords):
+        intersections = []
+        for box_coords in boxes_coords:
+            intersections.append(self.checkIntersect(box_coords))
+        if np.any(intersections): self.intersect = True
+        else: self.intersect = False
+
 if __name__ == '__main__':
     test = Detector()
+    y = Key(100, 100, 200, 200)
 
     while not test.breakLoop:
         test.readImage()
         test.detectHands()
         test.drawResults(hands=True, boxes=True)
+        y.checkPressed(test.boxes_coords)
+        y.draw(test.image)
         test.showImage()
         test.checkBreak(break_key='q')
